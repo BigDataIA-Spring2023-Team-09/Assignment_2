a183f4bc096622bf89e84b465cc7f303ad6dd55b
LakshmanRaaj <senthilnathan.l@northeastern.edu>
2023-02-19 14:29:14 -0500

    Added basic functions

--- fastapi/basic_func.py
+++ fastapi/basic_func.py
@@ -1,6 +1,25 @@
+from dotenv import load_dotenv
+import boto3
+import os
+import botocore
+import time
+
+s3client = boto3.client('s3', 
+                        region_name = 'us-east-1',
+                        aws_access_key_id = os.environ.get('AWS_ACCESS_KEY'),
+                        aws_secret_access_key = os.environ.get('AWS_SECRET_KEY')
+                        )
+
+#Establish connection to logs
+clientlogs = boto3.client('logs', 
+                        region_name = 'us-east-1',
+                        aws_access_key_id = os.environ.get('AWS_ACCESS_KEY'),
+                        aws_secret_access_key = os.environ.get('AWS_SECRET_KEY')
+                        )
 
 def say_hello():
     return "Hello World"
+
 
 def fetch_url(
     year=int,
@@ -10,7 +29,102 @@ def fetch_url(
 ):
     aws_nexrad_url = f"https://noaa-nexrad-level2.s3.amazonaws.com/index.html#{year:04}/{month:02}/{date:02}/{station:02}"
     return aws_nexrad_url
+    
 
 print(say_hello())
 func_op = fetch_url(2022, 6, 21, 'KAMX')
 print(func_op)
+
+
+def path_from_filename_goes(filename):
+
+    ind = filename.index('s')
+    file_path = f"ABI-L1b-RadC/{filename[ind+1: ind+5]}/{filename[ind+5: ind+8]}/{filename[ind+8: ind+10]}/{filename}"
+    return file_path
+
+
+# Generates file path in nexrad bucket from file name
+def path_from_filename_nexrad(filename):
+   
+    details_list =[]
+    details_list.append(filename[:4])
+    details_list.append(filename[4:8])
+    details_list.append(filename[8:10])
+    details_list.append(filename[10:12])
+    details_list.append(filename)
+    file_path = f"{details_list[1]}/{details_list[2]}/{details_list[3]}/{details_list[0]}/{details_list[4]}"
+    return file_path
+
+
+def check_if_file_exists_in_s3_bucket(bucket_name, file_name):
+    try:
+        s3client.head_object(Bucket=bucket_name, Key=file_name)
+        return True
+
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            return False
+        else:
+            raise
+
+
+def generate_download_link_goes(bucket_name, object_key, expiration=3600):
+    response = s3client.generate_presigned_url(
+        ClientMethod='get_object',
+        Params={
+            'Bucket': bucket_name,
+            'Key': object_key
+        },
+        ExpiresIn=expiration
+    )
+    write_logs_goes(f"{[object_key.rsplit('/', 1)[-1],response]}")
+    return response
+
+
+def generate_download_link_nexrad(bucket_name, object_key, expiration=3600):
+    response = s3client.generate_presigned_url(
+        ClientMethod='get_object',
+        Params={
+            'Bucket': bucket_name,
+            'Key': object_key
+        },
+        ExpiresIn=expiration
+    )
+    write_logs_nexrad(f"{[object_key.rsplit('/', 1)[-1],response]}")
+    return response
+
+
+#Generating logs with given message in cloudwatch
+def write_logs_goes(message : str):
+    clientlogs.put_log_events(
+    logGroupName = "assignment2-logs",
+    logStreamName = "goes-logs",
+    logEvents = [
+        {
+            'timestamp' : int(time.time() * 1e3),
+            'message' : message
+        }
+    ]                            
+    )
+
+
+#Generating logs with given message in cloudwatch
+def write_logs_nexrad(message : str):
+    clientlogs.put_log_events(
+    logGroupName = "assignment2-logs",
+    logStreamName = "nexrad-logs",
+    logEvents = [
+        {
+            'timestamp' : int(time.time() * 1e3),
+            'message' : message
+        }
+    ]                            
+    )
+
+
+def copy_to_public_bucket(src_bucket_name, src_object_key, dest_bucket_name, dest_object_key):
+    copy_source = {
+        'Bucket': src_bucket_name,
+        'Key': src_object_key
+    }
+    s3client.copy_object(Bucket=dest_bucket_name, CopySource=copy_source, Key=dest_object_key)
--- fastapi/main.py
+++ fastapi/main.py
@@ -3,10 +3,16 @@ from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 import boto3
 import base_model
+import streamlit as st
+from starlette.responses import HTMLResponse
+import Assignment_2.fastapi.basic_func as basic_func
+
 
 app =FastAPI()
 
+
 load_dotenv()
+
 
 s3client = boto3.client('s3', 
                         region_name = 'us-east-1',
@@ -14,27 +20,94 @@ s3client = boto3.client('s3',
                         aws_secret_access_key = os.environ.get('AWS_SECRET_KEY')
                         )
 
-@app.get("/say_hello")
+
+goes18_bucket = 'noaa-goes18'
+user_bucket_name = os.environ.get('USER_BUCKET_NAME')
+nexrad_bucket = 'noaa-nexrad-level2'
+
+
+@app.get("/say-hello")
 async def say_hello() -> dict:
     return {"message":"Hello World"}
 
 
-@app.post("/fetch_url_nexrad")
+@app.get("/streamlit")
+def streamlit_endpoint():
+    return st.embed_iframe("https://share.streamlit.io/streamlit/demo-uber-nyc-pickups/main/app.py")
+
+
+@app.post("/fetch-url-nexrad")
 async def fetch_url_nexrad(userinput: base_model.UserInputNexrad) -> dict:
     # if userinput.date > 31:
     #     return 400 bad request . return incorrect date
-    aws_url = f"https://noaa-nexrad-level2.s3.amazonaws.com/index.html#{userinput.year:04}/{userinput.month:02}/{userinput.date:02}/{userinput.station}"
-    
+    aws_url = f"https://noaa-nexrad-level2.s3.amazonaws.com/index.html#{userinput.year:04}/{userinput.month:02}/{userinput.date:02}/{userinput.station}"   
     return {'url': aws_url }
 
-@app.post("/fetch_url_goes")
+
+@app.post("/fetch-url-goes")
 async def fetch_url_goes(userinput: base_model.UserInputGOES) -> dict:
     # if userinput.date > 31:
     #     return 400 bad request . return incorrect date
-    aws_url = f"https://noaa-nexrad-level2.s3.amazonaws.com/index.html#{userinput.year:04}/{userinput.day:02}/{userinput.hour:02}"
+    aws_url = f"https://noaa-goes18.s3.amazonaws.com/index.html#{userinput.year:04}/{userinput.day:02}/{userinput.hour:02}"
     return {'url': aws_url }
 
-# goes18_bucket = 'noaa-goes18'
+
+@app.post("/fetch-url-goes-from-name")
+async def fetch_url_goes_from_name(userinput: base_model.UserInputName) -> dict:
+    # if userinput.date > 31:
+    #     return 400 bad request . return incorrect date
+
+    # Checks if the provided file exists in goes bucket
+    if basic_func.check_if_file_exists_in_s3_bucket(goes18_bucket, userinput.name):
+
+        # Generate file path from filename
+        src_object_key = basic_func.path_from_filename_goes(userinput.name)
+
+        # Define path where the file has to be written
+        user_object_key = f'logs/goes18/{userinput.name}'
+
+        # Copy file from GOES18 bucket to user bucket
+        basic_func.copy_to_public_bucket(goes18_bucket, src_object_key, user_bucket_name, user_object_key)
+
+        # Generate link from user bucket
+        aws_url = basic_func.generate_download_link(user_bucket_name, user_object_key)
+
+        # Returns the generated URL
+        return {'url': aws_url }
+    
+    else:
+        # Returns a message saying file does not exist in the bucket
+        return {"message":"404: File not found"}
+
+
+@app.post("/fetch-url-nexrad-from-name")
+async def fetch_url_nexrad_from_name(userinput: base_model.UserInputName) -> dict:
+    # if userinput.date > 31:
+    #     return 400 bad request . return incorrect date
+
+    # Checks if the provided file exists in nexrad bucket
+    if basic_func.check_if_file_exists_in_s3_bucket(nexrad_bucket, userinput.name):
+
+        # Generate file path from filename
+        src_object_key = basic_func.path_from_filename(userinput.name)
+
+        # Define path where the file has to be written
+        user_object_key = f'logs/nexrad/{userinput.name}'
+
+        # Copy file from GOES18 bucket to user bucket
+        basic_func.copy_to_public_bucket(nexrad_bucket, src_object_key, user_bucket_name, user_object_key)
+
+        # Generate link from user bucket
+        aws_url = basic_func.generate_download_link_nexrad(user_bucket_name, user_object_key)
+
+        # Returns the generated URL
+        return {'url': aws_url }
+    
+    else:
+        # Returns a message saying file does not exist in the bucket
+        return {"message":"404: File not found"}
+
+
 # user_bucket_name = os.environ.get('USER_BUCKET_NAME')
 # user_object_key = f'logs/goes18/{UserInputName.name}'
 
