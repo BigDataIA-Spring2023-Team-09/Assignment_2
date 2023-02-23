a189fb1994a5abc3b68af927d7a0491ffb5acad8
Harshit Mittal <hmittal15@gmail.com>
2023-02-23 17:18:32 -0500

    Implemented CI with pytest

--- fastapi/basic_func.py
+++ fastapi/basic_func.py
@@ -10,7 +10,21 @@ import time
 import requests
 import pandas as pd
 import csv
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import base_model
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from fastapi import Depends, FastAPI, HTTPException, status
 
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "fa5296715ded673b98da4a16672646ca2184ef4634fdedfeebfad085615b1ddc"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 2
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 load_dotenv()
 database_file_name = 'location.db'
@@ -281,6 +295,87 @@ def validate_file(filename):
         message="Valid file"
     return (message)
 
+def get_users_data():
+    db = sqlite3.connect('users.db')
+    cursor = db.cursor()
+    cursor.execute('''select * from users''')
+
+    # Fetch all the rows as a list of tuples
+    rows = cursor.fetchall()
+
+    # Convert the rows to a list of dictionaries
+    data = []
+    for row in rows:
+        # Create a dictionary with keys corresponding to the table column names
+        record = {
+            "username": row[0],
+            "full_name": row[1],
+            "email": row[2],
+            "hashed_password": row[3]
+        }
+        data.append(record)
+    return data
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    my_dict={}
+    for i in range(len(db)):
+        if (db[i]['username']==username):
+            data=get_users_data()
+            my_dict=data[i]
+    if my_dict:
+        return base_model.UserInDB(**my_dict)
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = base_model.TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    data=get_users_data()
+    user = get_user(data, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+async def get_current_active_user(current_user: base_model.User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
 # Clean up
 def conn_close(c):
